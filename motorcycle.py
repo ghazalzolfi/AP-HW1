2688a8ed84b7d57935c8d57551bf81b830f90ac5
ghazal <ghazalzolfi1383@gmail.com>
2023-04-02 11:58:27 +0330

    "add motorcycle class and attributes"

---
+++ motorcycle.py
@@ -0,0 +1,13 @@
+from vehicle import Vehicle
+class Motorcycle:
+    def __init__(self, transmission_type: str, mileage: int, is_running: bool, is_damaged: bool):
+        super().__init__(
+            model, year, color, price, weight
+        )
+        self.transmission_type = transmission_type
+        self.mileage = mileage
+        self.is_running = is_running
+        self.is_damaged = is_damaged
+
+
+
