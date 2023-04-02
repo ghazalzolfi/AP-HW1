34588033e47448240cf6a4c9fcf9fe40e015fc5e
ghazal <ghazalzolfi1383@gmail.com>
2023-04-02 11:00:59 +0330

    "add attributes and methods' name for plane class"

--- plane.py
+++ plane.py
@@ -1,1 +1,22 @@
 from vehicle import Vehicle
+
+class Plane(vehicle):
+    def __int__(self,passenger_capacity: int, is_flying: bool, current_location: str, aircraft_company: float, wing):
+        self.passenger_capacity = passenger_capacity
+        self.is_flying = is_flying
+        self.current_location = current_location
+        self.aircraft_company = aircraft_company
+        self.wing = wing
+
+    def take_off(self):
+        pass
+
+    def land(self):
+        pass
+
+    def fly_to(self,destination: str):
+        pass
+
+    def get_current_location(self):
+        pass
+
