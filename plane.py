ed10c051a9791c16168dcfa868416cb0432021df
ghazal <ghazalzolfi1383@gmail.com>
2023-04-02 11:57:58 +0330

    "add super().__init__"

--- plane.py
+++ plane.py
@@ -2,6 +2,9 @@ from vehicle import Vehicle
 
 class Plane(vehicle):
     def __int__(self,passenger_capacity: int, is_flying: bool, current_location: str, aircraft_company: float, wing):
+        super().__init__(
+            model, year, color, price, weight
+        )
         self.passenger_capacity = passenger_capacity
         self.is_flying = is_flying
         self.current_location = current_location
