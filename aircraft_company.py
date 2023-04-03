b62eb97a554cc96c26a14d7d0c414edaa20e9cff
ghazal <ghazalzolfi1383@gmail.com>
2023-04-03 21:47:09 +0330

    "add aircraft_company class"

---
+++ aircraft_company.py
@@ -0,0 +1,17 @@
+from plane import Plane
+class AircraftCompany:
+    def __init__(self, name, founded_year, address: string, planes):
+        self.name = name
+        self.founded_year = founded_year
+        self.address = address
+        self.planes = planes
+
+    def add_plane(self, plane):
+        pass
+
+    def remove_plane(self, plane):
+        pass
+
+    def get_planes(self, plane):
+        pass
+
