8380f774badb1ec17396d4c45c6026f614d9b341
ghazal <ghazalzolfi1383@gmail.com>
2023-04-03 23:17:04 +0330

    "add airline class"

---
+++ airline.py
@@ -0,0 +1,17 @@
+from plane import Plane
+class airline:
+    def __init__(self, name, country, code, flights, employees):
+        self.name = name
+        self.country = country
+        self.code = code
+        self.flights = flights
+        self.employees = employees
+
+    def add_flight(self, flight):
+        pass
+
+    def remove_flight(self, flight):
+        pass
+
+    def get_flights(self, date):
+        pass
