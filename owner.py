0ff15347b78fe860e0a7a8e91861eed2a4961478
ghazal <ghazalzolfi1383@gmail.com>
2023-04-04 11:43:48 +0330

    "add owner class"

---
+++ owner.py
@@ -0,0 +1,19 @@
+class Owner:
+    def __init__(self, name, address, phone_number, email):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.email = email
+
+    def add_car(self, car: str):
+        pass
+    def remove_car(self, car: str):
+        pass
+    def get_cars(self):
+        pass
+    def add_motorcycle(self, motorcycle: str):
+        pass
+    def remove_motorcycle(self, motorcycle: str):
+        pass
+    def get_motorcycles(self):
+        pass
