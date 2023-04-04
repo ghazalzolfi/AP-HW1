cb7d3c0faa66148d3442d0cf44e60e0334cb97f0
ghazal <ghazalzolfi1383@gmail.com>
2023-04-04 14:24:54 +0330

    "add engine class"

---
+++ engine.py
@@ -0,0 +1,15 @@
+class Engine:
+    def __init__(self, cylinders, horsepower, fuel_type, displacement):
+        self.cylinders = cylinders
+        self.horsepower = horsepower
+        self.fuel_type = fuel_type
+        self.displacement = displacement
+
+    def get_cylinders(self):
+        pass
+    def set_cylinders(self):
+        pass
+    def start(self):
+        pass
+    def stop(self):
+        pass
