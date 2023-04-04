b6d327d39a0b1644b68fa2c925d4af9245524230
ghazal <ghazalzolfi1383@gmail.com>
2023-04-04 14:39:06 +0330

    "add customer class"

---
+++ customer.py
@@ -0,0 +1,15 @@
+class Customer:
+    def __init__(self, customer_ID, name, address, phone_number, license_num):
+        self.customer_ID = customer_ID
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.license_num = license_num
+
+    def make_reservation(self):
+        pass
+    def cancel_reservation(self):
+        pass
+
+    def view_transactions(self):
+        pass
