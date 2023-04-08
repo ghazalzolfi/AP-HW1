a463bde07ea7e4aca08f15f14ce74b0c8ad714e8
ghazal <ghazalzolfi1383@gmail.com>
2023-04-08 22:42:25 +0330

    "update methods of customer class"

--- customer.py
+++ customer.py
@@ -1,15 +1,15 @@
 class Customer:
     def __init__(self, customer_ID, name, address, phone_number, license_num):
-        self.customer_ID = customer_ID
-        self.name = name
-        self.address = address
-        self.phone_number = phone_number
-        self.license_num = license_num
+        self._customer_ID = customer_ID
+        self._name = name
+        self.__address = address
+        self.__phone_number = phone_number
+        self._license_num = license_num
 
     def make_reservation(self):
-        pass
+        print("Reservation made!")
     def cancel_reservation(self):
-        pass
+        print("Reservation cancled!")
 
     def view_transactions(self):
         pass
