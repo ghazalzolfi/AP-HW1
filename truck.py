5dada50ff1937600d458950db0d2e562f54161f4
ghazal <ghazalzolfi1383@gmail.com>
2023-04-12 10:53:42 +0330

    "update truck class"

--- truck.py
+++ truck.py
@@ -1,6 +1,6 @@
 from vehicle import Vehicle
 class Truck(Vehicle):
-    def __init__(self, license_plate, fuel_capacity, current_load, transmission_type):
+    def __init__(self, license_plate: str, fuel_capacity: float, current_load: float, transmission_type: str):
         super().__init__(
             model, year, color, price, weight, num_wheels
         )
@@ -9,15 +9,14 @@ class Truck(Vehicle):
         self.current_load  = current_load
         self.transmission_type = transmission_type
 
-    def load(self, weight):
-        pass
-    def unload(self, weight):
-        pass
-    def check_fuel(self):
-        pass
+    def load(self, weight: float):
+        self.current_load += weight
+        print(f"Loading! Your current load is {self.current_load}.")
+    def unload(self, weight: float):
+        self.current_load -= weight
+        print(f"Unloading! Your current load is {self.current_load}!")
     def check_load(self):
-        pass
-
+        return self.current_load
     # overriding abstract method
     def get_num_wheels(self):
         print("I may have four to eighteen wheels!")
