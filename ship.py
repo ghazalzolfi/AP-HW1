ab4fd6c1a64f55317353f3ec32665012dc87cf13
ghazal <ghazalzolfi1383@gmail.com>
2023-04-12 16:15:26 +0330

    "add ship class"

---
+++ ship.py
@@ -0,0 +1,19 @@
+from vehicle import Vehicle
+class Ship(Vehicle):
+    def __init__(self, captain, fuel_efficiency, is_moving, safety_rating):
+        super().__init__(
+            model, year, color, price, weight, num_wheels
+        )
+        self.captain = captain
+        self.fuel_efficiency = fuel_efficiency
+        self.is_moving = is_moving
+        self.safety_rating = safety_rating
+
+    def get_safty_rating(self):
+        return self.safety_rating
+    def set_safty_rating(self, safety_rating):
+        self.safety_rating = safety_rating
+        return self.safety_rating
+    def set_is_moving(self, is_moving):
+        self.is_moving = is_moving
+        return self.is_moving
