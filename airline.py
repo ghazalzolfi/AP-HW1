5976f95abfbcd071735639863724cabdbf6e84a9
ghazal <ghazalzolfi1383@gmail.com>
2023-04-12 13:29:17 +0330

    "update airline class "

--- airline.py
+++ airline.py
@@ -1,17 +1,19 @@
-from plane import Plane
+from flight import Flight
 class airline:
-    def __init__(self, name, country, code, flights, employees):
+    def __init__(self, name, country, code, flights: list, employees):
         self.name = name
         self.country = country
         self.code = code
         self.flights = flights
         self.employees = employees
 
-    def add_flight(self, flight):
-        pass
+    def add_flight(self, flight: Flight):
+        self.flights.append(flight)
+        print(f"{flight} is added to {self} airline!")
 
-    def remove_flight(self, flight):
-        pass
+    def remove_flight(self, flight: Flight):
+        self.flights.remove(flight)
+        print(f"{flight} is removed from {self} airline!")
 
-    def get_flights(self, date):
-        pass
+    def get_flights(self):
+        return self.flights
