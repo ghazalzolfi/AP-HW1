aaaf767b186855a369b9fad43a17c6e03cf1aa10
ghazal <ghazalzolfi1383@gmail.com>
2023-04-12 13:24:20 +0330

    "update flight class "

--- flight.py
+++ flight.py
@@ -1,6 +1,6 @@
 from plane import Plane
 class flight:
-    def __init__(self, number, airplane_type, departure_time, arrival_time, status, passengers):
+    def __init__(self, number, airplane_type, departure_time, arrival_time, status, passengers: list):
         self.number = number
         self.airplane_type = airplane_type
         self.departure_time = departure_time
@@ -9,16 +9,16 @@ class flight:
         self.passengers = passengers
 
     def add_passengers(self, passenger):
-        pass
+        self.passengers.append(passenger)
+        print(f"{passenger} was added to passengers!")
 
     def remove_passengers(self, passenger):
-        pass
+        self.passengers.remove(passenger)
+        print(f"{passenger} was remove from passengers!")
 
     def get_available_seats(self):
         pass
 
     def get_flight_duration(self):
-        pass
-
-    def get_flight_distance(self):
-        pass
+        self.flight_duration = self.arrival_time - self.departure_time
+        return  self.flight_duration
