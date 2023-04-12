8cdffe3313bdf2f073c099aadc5f62a4bedd0352
ghazal <ghazalzolfi1383@gmail.com>
2023-04-12 11:12:31 +0330

    "update aircraft_company class "

--- aircraft_company.py
+++ aircraft_company.py
@@ -1,17 +1,18 @@
-from plane import Plane
 class AircraftCompany:
-    def __init__(self, name, founded_year, address: string, planes):
+    def __init__(self, name, founded_year, address: str, planes: list):
         self.name = name
         self.founded_year = founded_year
         self.address = address
         self.planes = planes
 
     def add_plane(self, plane):
-        pass
+        self.planes.append(plane)
+        print(f"{plane} added!")
 
     def remove_plane(self, plane):
-        pass
+        self.planes.remove(plane)
+        print(f"{plane} removed!")
 
-    def get_planes(self, plane):
-        pass
+    def get_planes(self):
+        return self.planes
 
