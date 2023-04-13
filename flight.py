fdf67e998f65fd124ad3dbc7581aaf545f85fc92
ghazal <ghazalzolfi1383@gmail.com>
2023-04-13 10:30:05 +0330

    "update get_available_seats method"

--- flight.py
+++ flight.py
@@ -1,12 +1,13 @@
 from plane import Plane
 class flight:
-    def __init__(self, number, airplane_type, departure_time, arrival_time, status, passengers: list):
+    def __init__(self, number, airplane_type, departure_time, arrival_time, status, passengers: list, passenger_capacity):
         self.number = number
         self.airplane_type = airplane_type
         self.departure_time = departure_time
         self.arrival_time = arrival_time
         self.status = status
         self.passengers = passengers
+        self.plane = Plane(passenger_capacity)
 
     def add_passengers(self, passenger):
         self.passengers.append(passenger)
@@ -17,8 +18,9 @@ class flight:
         print(f"{passenger} was remove from passengers!")
 
     def get_available_seats(self):
-        pass
+        available_seats_num = self.plane.passenger_capacity - len(self.passengers)
+        print(f"There are {available_seats_num} empty seats!")
 
     def get_flight_duration(self):
-        self.flight_duration = self.arrival_time - self.departure_time
-        return  self.flight_duration
+        flight_duration = self.arrival_time - self.departure_time
+        return  flight_duration
