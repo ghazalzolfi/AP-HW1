092b09831cdd797da94648a8b0d8b97d2d3e0dcf
ghazal <ghazalzolfi1383@gmail.com>
2023-04-14 00:46:23 +0330

    "updated"

--- owner.py
+++ owner.py
@@ -1,3 +1,5 @@
+from car import Car
+from motorcycle import Motorcycle
 class Owner:
     def __init__(self, name, address, phone_number, email, **kwargs):
         self.name = name
@@ -9,16 +11,16 @@ class Owner:
         self.cars = []
         self.motorcycles = []
 
-    def add_car(self, car: str):
+    def add_car(self, car: Car):
         self.cars.append(car)
         return self.cars
-    def remove_car(self, car: str):
+    def remove_car(self, car: Car):
         self.cars.remove(car)
         return self.cars
 
-    def add_motorcycle(self, motorcycle: str):
+    def add_motorcycle(self, motorcycle: Motorcycle):
         self.motorcycles.append(motorcycle)
         return self.motorcycles
-    def remove_motorcycle(self, motorcycle: str):
+    def remove_motorcycle(self, motorcycle: Motorcycle):
         self.motorcycles.remove(motorcycle)
         return self.motorcycles
