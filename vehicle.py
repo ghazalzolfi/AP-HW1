56634a75ab1280ddf8623d4bc85b79a13c36631c
ghazal <ghazalzolfi1383@gmail.com>
2023-04-14 13:51:06 +0330

    "update vehicle class"

--- vehicle.py
+++ vehicle.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+
+
 class Vehicle(ABC):
     def __init__(self, model: str, year: int, color: str, price: float, weight: float, num_wheels: int, **kwargs):
         self.model = model
@@ -9,13 +11,14 @@ class Vehicle(ABC):
         self.num_wheels = num_wheels
         self.speed = 0
         self.fuel = 0
-        #the constructor accepts any additional keyword arguments.
+#       the constructor accepts any additional keyword arguments.
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def __str__(self):
         return f"{self.model} {self.year} {self.color}"
-    def accelerate(self, final_speed : float):
+
+    def accelerate(self, final_speed: float):
         self.speed = final_speed
         print(f"accelerating! Your speed is {self.speed}")
 
@@ -23,20 +26,22 @@ class Vehicle(ABC):
         self.speed = final_speed
         print(f"braking! Your speed is {self.speed}")
 
-    def honk_horn(self):
+    @staticmethod
+    def honk_horn():
         print("Honking!")
 
     def refuel(self, amount: float):
         self.fuel += amount
         print(f"refueling!")
 
-    # polymorphism (defining methods in parent class)
+#   polymorphism (defining methods in parent class)
     def start(self):
         print("Rٍٍeady to go!")
+
     def stop(self):
         print("Stopping!")
 
-    #defining abstract method in parent class
+#   defining abstract method in parent class
     @abstractmethod
     def get_num_wheels(self):
         pass
