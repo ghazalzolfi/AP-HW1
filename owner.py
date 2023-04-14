7c21ba2728b6b2cd73fb1e0a2a3eb704ca71651f
ghazal <ghazalzolfi1383@gmail.com>
2023-04-14 12:35:46 +0330

    "update owner class"

--- owner.py
+++ owner.py
@@ -1,5 +1,7 @@
 from car import Car
 from motorcycle import Motorcycle
+
+
 class Owner:
     def __init__(self, name, address, phone_number, email, **kwargs):
         self.name = name
@@ -11,20 +13,37 @@ class Owner:
             setattr(self, key, value)
         self.cars = []
         self.motorcycles = []
-        # aggregation
-        self.car = Car()
-        self.motorcycle = Motorcycle()
 
     def add_car(self, car: Car):
         self.cars.append(car)
         return self.cars
-    def remove_car(self, car: Car):
-        self.cars.remove(car)
-        return self.cars
+
+    def __find_car(self, car_model):
+        founded_car = None
+        for car in self.cars:
+            if car.model == car_model:
+                founded_car = car
+        return founded_car
+
+    def remove_car(self, car_model):
+        founded_car = self.__find_car(car_model)
+        if founded_car:
+            self.cars.remove(founded_car)
+        return founded_car
 
     def add_motorcycle(self, motorcycle: Motorcycle):
         self.motorcycles.append(motorcycle)
         return self.motorcycles
-    def remove_motorcycle(self, motorcycle: Motorcycle):
-        self.motorcycles.remove(motorcycle)
-        return self.motorcycles
+
+    def __find_motorcycle(self, motorcycle_model):
+        founded_motorcycle = None
+        for motorcycle in self.motorcycles:
+            if motorcycle.model == motorcycle_model:
+                founded_motorcycle = motorcycle
+        return founded_motorcycle
+
+    def remove_motorcycle(self, motorcycle_model):
+        founded_motorcycle = self.__find_motorcycle(motorcycle_model)
+        if founded_motorcycle:
+            self.motorcycles.remove(founded_motorcycle)
+        return founded_motorcycle
